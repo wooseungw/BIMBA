551be56c9f4cb2cac534a211813cdf7336882ef8
wooseungw <woow070840@gmail.com>
2025-07-19 15:35:44 +0900

    Eval_BIMBA_LLaVA_Qwen2-7B 캡션 스크립트에서 불필요한 데이터셋 호출 제거 및 NextQA 데이터셋 처리 로직 수정

--- scripts/video/eval/Eval_BIMBA_LLaVA_Qwen2_7B_caption.sh
+++ scripts/video/eval/Eval_BIMBA_LLaVA_Qwen2_7B_caption.sh
@@ -8,43 +8,6 @@ model_name="withcaption"
 
 results_dir=results/BIMBA-LLaVA-Qwen2-7B_caption
 
-dataset_name=VideoMME
-python llava/eval/infer.py \
-    --model_path $model_path \
-    --model_base $model_base \
-    --model_name $model_name \
-    --results_dir ${results_dir}/${dataset_name} \
-    --max_frames_num 64 \
-    --dataset_name $dataset_name \
-    --video_root "path_to_video_folder" \
-    --data_path DATAS/eval/VideoMME/formatted_dataset.json \
-    --cals_acc
-
-
-dataset_name=MLVU
-python llava/eval/infer.py \
-    --model_path $model_path \
-    --model_base $model_base \
-    --model_name $model_name \
-    --results_dir ${results_dir}/${dataset_name} \
-    --max_frames_num 64 \
-    --dataset_name $dataset_name \
-    --data_path DATAS/eval/MLVU/formatted_dataset.json \
-    --video_root "path_to_video_folder" \
-    --cals_acc
-
-dataset_name=LongVideoBench
-python llava/eval/infer.py \
-    --model_path $model_path \
-    --model_base $model_base \
-    --model_name $model_name \
-    --results_dir ${results_dir}/${dataset_name}_val \
-    --max_frames_num 64 \
-    --dataset_name $dataset_name \
-    --data_path DATAS/eval/LongVideoBench/formatted_dataset.json \
-    --video_root "path_to_video_folder" \
-    --cals_acc
-
 dataset_name=NextQA
 python llava/eval/infer.py \
     --model_path $model_path \
@@ -53,59 +16,9 @@ python llava/eval/infer.py \
     --results_dir ${results_dir}/${dataset_name}_test \
     --max_frames_num 64 \
     --dataset_name $dataset_name \
-    --data_path DATAS/eval/NextQA/formatted_dataset_test.json \
-    --video_root "path_to_video_folder" \
+    --data_path DATAS/eval/NextQA/formatted_test.json \
+    --video_root "DATAS/eval/NextQA/NExTVideo/" \
     --cals_acc
-
-dataset_name=PerceptionTest
-python llava/eval/infer.py \
-    --model_path $model_path \
-    --model_base $model_base \
-    --model_name $model_name \
-    --results_dir ${results_dir}/${dataset_name} \
-    --max_frames_num $max_frames_num \
-    --dataset_name $dataset_name \
-    --data_path DATAS/eval/PerceptionTest/formatted_dataset_val.json \
-    --video_root "path_to_video_folder" \
-    --cals_acc
-
-dataset_name=VNBench
-python llava/eval/infer.py \
-    --model_path $model_path \
-    --model_base $model_base \
-    --model_name $model_name \
-    --results_dir ${results_dir}/${dataset_name} \
-    --max_frames_num 128 \
-    --dataset_name $dataset_name \
-    --data_path DATAS/eval/VNBench/formatted_dataset_4try.json \
-    --video_root "path_to_video_folder" \
-    --cals_acc
-
-#EgoSchema subset
-dataset_name=EgoSchema
-python llava/eval/infer.py \
-    --model_path $model_path \
-    --model_base $model_base \
-    --model_name $model_name \
-    --results_dir ${results_dir}/${dataset_name} \
-    --max_frames_num 64 \
-    --dataset_name $dataset_name \
-    --data_path DATAS/eval/EgoSchema/formatted_dataset_val.json \
-    --video_root "path_to_video_folder" \
-    --cals_acc
-
-#EgoSchema full-set
-dataset_name=EgoSchema
-python llava/eval/infer.py \
-    --model_path $model_path \
-    --model_base $model_base \
-    --model_name $model_name \
-    --results_dir ${results_dir}/${dataset_name} \
-    --max_frames_num 64 \
-    --dataset_name $dataset_name \
-    --data_path DATAS/eval/EgoSchema/formatted_dataset_test.json \
-    --video_root "path_to_video_folder"
-
 
 # python llava/eval/submit_ego_schema.py
 # kaggle competitions submit -c egoschema-public -f results/BIMBA-LLaVA-Qwen2-7B/EgoSchema/es_submission.csv -m "BIMBA-LLaVA-Qwen2-7B"
