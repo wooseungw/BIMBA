020e1a14a2c6624bcc44c8909a15bd07bfe80e17
wooseungw <woow070840@gmail.com>
2025-07-19 16:02:13 +0900

    Eval_BIMBA_LLaVA_Qwen2_7B_caption.sh에서 모델 경로 수정 및 모델 베이스 주석 처리

--- scripts/video/eval/Eval_BIMBA_LLaVA_Qwen2_7B_caption.sh
+++ scripts/video/eval/Eval_BIMBA_LLaVA_Qwen2_7B_caption.sh
@@ -2,8 +2,8 @@ export CUDA_VISIBLE_DEVICES=1
 export WANDB_API_KEY=9fd21364ed6c1c6677a250972c5e19a931171974
 export PYTHONPATH="$PYTHONPATH:/home/tera/workspace/lcvlm/bimba_fork/llava"
 
-model_path="checkpoints/BIMBA-LLaVA-Qwen2-7B"
-model_base="lmms-lab/LLaVA-Video-7B-Qwen2"
+model_path="/work_dirs/withcaption/"
+# model_base="lmms-lab/LLaVA-Video-7B-Qwen2"  # LoRA가 아닌 경우 주석 처리
 model_name="withcaption"
 
 results_dir=results/BIMBA-LLaVA-Qwen2-7B_caption
@@ -11,7 +11,6 @@ results_dir=results/BIMBA-LLaVA-Qwen2-7B_caption
 dataset_name=NextQA
 python llava/eval/infer.py \
     --model_path $model_path \
-    --model_base $model_base \
     --model_name $model_name \
     --results_dir ${results_dir}/${dataset_name}_test \
     --max_frames_num 64 \
